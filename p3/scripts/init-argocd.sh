546a942f405f6f10e9726223f3c675557272662f
Ivan Petrov <ivanpetrov.main@gmail.com>
2026-05-14 03:39:27 -0600

    feat: add working version

--- p3/scripts/init-argocd.sh
+++ p3/scripts/init-argocd.sh
@@ -7,12 +7,18 @@ sudo kubectl create namespace dev
 # install argocd
 sudo kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# whait when argocd start to work
+sudo kubectl -n argocd wait --for=condition=Established --timeout=120s \
+  crd/applications.argoproj.io crd/appprojects.argoproj.io
+sudo kubectl -n argocd rollout status deploy/argocd-server --timeout=180s
+
 # bind cluster to git repo
-sudo kubectl apply -f ./confs/deployment.yml
+sudo kubectl apply -f ../confs/deployment.yml
 
 # expose argocd-server webui from pod to node
 # sudo kubectl port-forward svc/argocd-server -n argocd 8181:443 > /dev/null 2>&1 &
 
-# config argocd to work behind /argocd prefix and via port 80
-sudo kubectl patch deployment argocd-server -n argocd --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--rootpath=/argocd"}]'
-sudo kubectl patch deployment argocd-server -n argocd --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/0/command/-", "value": "--insecure"}]'
+# config argocd to work behind /argocd prefix
+sudo kubectl -n argocd patch configmap argocd-cmd-params-cm \
+  --type merge -p '{"data":{"server.insecure":"true","server.rootpath":"/argocd"}}'
+sudo kubectl -n argocd rollout restart deploy argocd-server
--- p3/scripts/init-cluster.sh
+++ p3/scripts/init-cluster.sh
@@ -12,9 +12,10 @@ rm get-docker.sh
 # install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
 
 # install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 # init cluster in containers, create single Node with Control and Worker same time
-sudo k3d cluster create ipetrov
+sudo k3d cluster create ipetrov -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"
